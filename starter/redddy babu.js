3a9c9edfb53c0fcabc33acd7fb01843f44ef24f0
reddybab <122990875+reddybab@users.noreply.github.com>
2023-01-18 20:23:52 +0530

    Add files via upload

---
+++ starter/redddy babu.js
@@ -0,0 +1,16 @@
+let myName = "reddybabu";
+let lastName = "pasupuleti";
+let rollNo = 9;
+console.log(myName);
+console.log(lastName);
+console.log(rollNo);
+alert("hi");
+const villageCount = 30;
+const requiredVillageCount = 40;
+console.log(villageCount, requiredVillageCount);
+if (requiredVillageCount >= villageCount) {
+  console.log("right");
+} else {
+  console.log("wrong");
+ const nameOfThe="village";
+ console.log(nameOfThe);
---
+++ starter/script.js
@@ -0,0 +1,365 @@
+/*let js = "amzing";
+console.log("rama raja mouli");
+console.log("reddybabu");
+console.log("babu reddy");
+console.log(23 + 23);
+let firstName = "kavitha";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+let country = "india";
+let continent = "asia";
+let population = "145m";
+console.log(country);
+console.log(continent);
+console.log(population);
+let isIsland = "srilanka";
+let language = "telugu";
+console.log(language);
+console.log(isIsland);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof "javascriptIsFun");
+console.log(typeof 89);
+console.log(typeof "java scriptisnumber");
+console.log(typeof "javascriptIsFun");
+console.log(typeof true);
+javascriptIsFun = "YES!";
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(year);
+console.log(typeof year);
+console.log(typeof null);
+let age = 30;
+age = 31;
+const birthYear = 1990;
+const job;
+var job = "programmer";
+job = "teacher";
+
+const now = 2037;
+const ageReddy = now - 2001;
+const ageSarah = now - 2018;
+
+console.log(ageReddy, ageSarah);
+
+console.log(ageReddy * 2, ageSarah * 2);
+testdata1://
+
+let marksWeight1 = 78;
+let marksHeight1 = 1.68;
+let johnsWeight1 = 92;
+let johnsHeight1 = 1.95;
+
+//testdata2://
+let marksWeight2 = 95;
+let marksHeight2 = 1.88;
+let jhonsWeight2 = 85;
+let johnsHeight2 = 1.88;
+
+//2. answer//
+
+ BMI=marksWeight/marksHeight**2=marksWeight1/(marksHeight1*marksHeight1)
+
+ console.log(BMI);/
+ */ /*
+const myAge = 2001;
+
+const reddyAge = 2021 - myAge;
+console.log(reddyAge);
+const hisAge = 2002;
+const raviAge = 2021 - hisAge;
+
+console.log(raviAge);
+console.log(reddyAge > raviAge);
+
+const fristName = "reddy";
+const lastName = "babu";
+const birthYear = 2001;
+const nowYear = 2021;
+console.log(
+  "my" +
+    "" +
+    "name" +
+    "" +
+    "is" +
+    fristName +
+    lastName +
+    "ihave" +
+    "" +
+    21 +
+    "years"
+); /* /N/USE FOR  NEXT LINE IN JAVA SCRIPT              */
+/*const reddyNew = ` my name is ${fristName}${lastName} i       have 21 years`;
+console.log(`my Name is redddybabu
+i have 21 years old`);
+const age = 12;
+
+const requiredAge = age >= 18;
+if (requiredAge) {
+  console.log(`this is required age for drive the vehicle`);
+} else {
+  console.log(`this is not right age for driving the vehicle`);
+}
+ this is example part
+const money = 100;
+const requiredMoney = money >= 120;
+if (requiredMoney) {
+  console.log(`you can get biryani for this money`);
+} else {
+  console.log(`you need extra amount for getting biryani`);
+}
+const birthYear = 1991;
+let centure;
+if (birthYear <= 2000) {
+  centure = 20;
+} else {
+  centure = 19;
+}
+console.log(centure);
+
+const date = 1990;
+if ((date) => 2000) {
+  console.log(`20thcenture`);
+} else {
+  console.log(`21th centure`);
+}
+this is a practice
+console.log("this is /N/reddybabu");
+const name = "reddybabu";
+console.log(name);
+alert("hi");
+let hi = "this is reddybabu";
+*/
+/*falsy values  are : 0,null,undefined,NaN,""*/
+/*const money = 0;
+if (money) {
+  console.log("don't spent extra money");
+} else {
+  console.log("you can get job");
+}
+const passMarks = 1;
+if (passMarks) {
+  console.log("you need 24 marks");
+} else {
+  console.log("you need practice more");
+}
+const numberValue = 0;
+if (numberValue) {
+  console.log("this is dont called as a zero");
+} else {
+  console.log("we can callled as a zero");
+}
+console.log(
+  "10000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+);
+let height;
+if (height) {
+  console.log("hey height is defined");
+} else {
+  console.log("height is un defiend");
+}
+prompt("this is reddybabu");
+const name = prompt("my name is reddy babu");
+if (name === "my name is reddy babu") {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+*/
+// let discuss about and operators in logical
+//
+/*const age = 1;
+if (age = 21 and age=1) {
+  console.log("you are eligible to marry a person");
+} else {
+  console.log("you are not eligible marrige");
+}*/
+/*const iHaveGoodPatient = true; //a
+const iHaveGoodTalent = true; //b
+console.log(iHaveGoodPatient && iHaveGoodTalent);
+if (iHaveGoodPatient && iHaveGoodTalent) {
+  console.log("so you can get a job in future");
+} else {
+  console.log("you want to improve your skills");
+}
+console.log(iHaveGoodPatient && iHaveGoodTalent);
+const iGlad = false; //c
+console.log(iHaveGoodPatient && iHaveGoodTalent);
+console.log(iHaveGoodPatient || iHaveGoodTalent);
+console.log(!iHaveGoodTalent);
+if (iHaveGoodPatient && iHaveGoodTalent && iGlad) {
+  console.log("you can get a job");
+} else {
+  console.log("you want to improve your skills");
+}
+const biryaniIsGood = true; //1
+const rateIsAlsoGood = true; //2
+const surviingAlsoGood = true; //3
+if (biryaniIsGood && rateIsAlsoGood && !surviingAlsoGood) {
+  console.log("this is a good restuarant");
+} else {
+  console.log("next time we can go onother restuarant ");
+}
+const dolphinScore = (96 + 89 + 108) / 3; //1
+console.log(dolphinScore);
+const kolasScore = (88 + 91 + 110) / 3; //2
+console.log(kolasScore);
+if (dolphinScore >= kolasScore) {
+  console.log("dolphins are the winners");
+} else {
+  console.log("kolas are the winners");
+}
+const minimumScore = 100;*/
+/*const dolphinScore = (97 + 112 + 101) / 3; //1
+console.log(dolphinScore);
+const kolasScore = (109 + 95 + 123) / 3; //2
+console.log(kolasScore);
+if (dolphinScore >= kolasScore && dolphinScore >= 100) {
+  console.log("dolphins are the winners");
+} else if (kolasScore >= dolphinScore && kolasScore >= 100) {
+  console.log("kolas are the winners");
+}
+const minimumScore = 100;*/
+/*const dolphinScore = (97 + 112 + 89) / 3; //1
+console.log(dolphinScore);
+const kolasScore = (109 + 96 + 89) / 3; //2
+console.log(kolasScore);
+if (dolphinScore > kolasScore && dolphinScore > 100) {
+  console.log("dolphins are the winners");
+} else if (kolasScore > dolphinScore && kolasScore > 100) {
+  console.log("kolas are the winners");
+} else if (dolphinScore && kolasScore >= 100) {
+  console.log("both are the winners");
+} else {
+  console.log("both are not win");
+}*/
+/*for example there is two secniours
+reddybabu score=89
+lavakumar score=90
+minimum score=91
+total score=100
+who is the winner
+*/ /*
+const reddyBabuScore = 89;
+const lavaKumarScore = 90;
+console.log(reddyBabuScore, lavaKumarScore);
+if (reddyBabuScore > lavaKumarScore && reddyBabuScore >= 91) {
+  console.log("reddybabu is the first");
+} else if (lavaKumarScore > reddyBabuScore && lavaKumarScore >= 91) {
+  console.log("lavakumar is the winner");
+} else {
+  console.log("both are not the winners");
+}
+let day = "tuesday";
+switch (day) {
+  case "monday":
+    console.log("i will   go to  company");
+    break;
+  case "tuesday":
+    console.log("i wii go to temple");
+    break;
+  case "wendsday":
+    console.log("i wii to bus stand");
+    break;
+  case "thursday":
+    console.log("iwill go park");
+    break;
+  case "friday":
+  case "saturday":
+  case "sunday":
+    console.log("i willl go musium");
+    break;
+  default:
+    console.log("i will take rest");
+}*
+const day = "black day";
+if (day === "tuesday") {
+  console.log("i will go theater");
+} else if (day === "wendsday") {
+  console.log("i will take leave");
+} else {
+  console.log("i eat food ");
+}*/
+const myCareer = "agbsc";
+switch (myCareer) {
+  case "agbsc":
+    console.log("i will choose ao post");
+    break;
+  case "software engineer":
+    console.log("i will chose front end devoloper");
+    break;
+  default:
+    console.log("i will continue as bpo");
+}
+const price = 60 / 2;
+switch (price) {
+  case 30:
+    console.log("i will get noodles");
+    break;
+  case 60:
+    console.log("i will get pizza");
+    break;
+  case 100:
+    console.log("i will get biryani");
+    break;
+  default:
+    console.log("i didn't buy any thing ");
+}
+/* conditional operator
+const age = 22;
+age >= 18
+  ? console.log("you are eligible to get marry some one")
+  : console.log("you can wait until time arrives");
+*/ /*
+const age = 22;
+const drink = age >= 18 ? "wine"|| "water" : console.log("wine":"water");
+*/
+/*const age = 189;
+age >= 18
+  ? console.log("you can drink wine")
+  : console.log("i want to drink water");
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);*/ /*
+const bike = 11111;
+bike >= 100000
+  ? console.log("you can get the bike")
+  : console.log("you can't get the  the bike");
+  */ /*
+const keyBoard = 90000;
+if (keyBoard >= 1000) {
+  console.log("you can buy this key board");
+} else {
+  console.log("you cant get the keyboard");
+}
+switch (keyBoard) {
+  case "1000":
+    console.log("you can get the key board");
+    break;
+  default:
+    console.log("you cant get the keyboard");
+}
+*/
+let age = 2;
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+console.log(`i would like drink ${age >= 18 ? "wine" : "water"}`);
+console.log(`this is reddy babu  i have ${18 - 13}fingers on my hand`);
+const bill = 275;
+const tip = bill >= 50 && bill >= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `the bill was ${bill} and the tip is${tip}and the total bill is ${bill + tip}`
+);
